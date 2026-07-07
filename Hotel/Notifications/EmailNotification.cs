0e015e251035e322e195a7dc60856da444330048
Vitalii <vitalik.huda@gmail.com>
2026-07-07 23:32:30 +0300

    Completed Task 3: Added Email and SMS notifications

---
+++ Hotel/Notifications/EmailNotification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel.Notifications
+{
+    public class EmailNotification : NotificationBase, INotificationChannel
+    {
+
+        public void'
+
+    }
+}
---
+++ Hotel/Notifications/NotificationBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel.Notifications
+{
+    public abstract class NotificationBase
+    {
+
+        public abstract void Send(string message);
+
+        protected string FormatMessage(string message) => $"[Booking] {message}";
+
+    }
+
+
+    public interface INotificationChannel
+    {
+        void Send(string message);
+    }
+}
