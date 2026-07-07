e8664637a1140cf5d8a05c95eb26d598988a22b3
Vitalii <vitalik.huda@gmail.com>
2026-07-07 11:16:29 +0300

    feat: task1 record/struct/class

---
+++ Hotel/Models/Booking.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Booking
+{
+    public Guid Id { get; init; }
+    public BookingRequest Request { get; init; } = null!;
+    public BookingStatus Status { get; private set; } = BookingStatus.Pending;
+    public void Confirm() => Status = BookingStatus.Confirmed;
+    public void Cancel() => Status = BookingStatus.Cancelled;
+}
+public enum BookingStatus { Pending, Confirmed, Cancelled }
---
+++ Hotel/Models/BookingRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public record BookingRequest
+{
+
+
+    public Guid RoomId { get; init; }
+    public DateOnly CheckIn { get; init; }
+    public DateOnly CheckOut { get; init; }
+    public int GuestsCount { get; init; }
+
+
+
+    public BookingRequest(Guid roomId, DateOnly checkIn, DateOnly checkOut, int guestsCount)
+
+    {
+        // тут твої if-перевірки
+
+        if (guestsCount <= 0) {
+        throw new ArgumentException("Кількість гостей має бути більше нуля");
+}
+
+        if (checkIn >= checkOut)
+        {
+            throw new ArgumentException();
+        }
+
+        RoomId = roomId;
+        CheckIn = checkIn;
+        CheckOut = checkOut;
+        GuestsCount = guestsCount;
+    }
+}
