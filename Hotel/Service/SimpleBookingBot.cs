ac1b550391632cac9c1f9368e224253510dcd715
Vitalii <vitalik.huda@gmail.com>
2026-07-08 13:45:29 +0300

    feat: applied ISP, split IBookingOperations and implemented SimpleBookingBot

---
+++ Hotel/Service/SimpleBookingBot.cs
@@ -0,0 +1,22 @@
+﻿using Hotel.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel.Service
+{
+    public class SimpleBookingBot: IBookingCreation, IBookingCancellation
+    {
+
+        public void Create(BookingRequest request)
+        {
+            Console.WriteLine("The booking has been created");
+        }
+
+        public void Cancel(Guid bookingId)
+        {
+            Console.WriteLine("The booking has been cancelled");
+        }
+
+    }
+}
